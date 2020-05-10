625849459df5c30aa159a385b8f2153f41ff0d7a
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-05-10 13:46:08 +0300

    * add adapter

--- app/src/main/java/com/example/jobforstudent/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/jobforstudent/ui/search/SearchAdapter.kt
@@ -1,2 +1,28 @@
 package com.example.jobforstudent.ui.search
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jobforstudent.R
+import com.example.jobforstudent.ui.search.SearchAdapter.SearchViewHolder
+
+class SearchAdapter(private val data: List<String>) : RecyclerView.Adapter<SearchViewHolder>() {
+
+    class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textView: TextView = itemView.findViewById(R.id.item_advert_company_name_text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_advert, parent, false)
+        return SearchViewHolder(itemView)
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.textView.text = data[position]
+    }
+
+}
--- app/src/main/java/com/example/jobforstudent/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/jobforstudent/ui/search/SearchFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jobforstudent.R
+import com.example.jobforstudent.CompanyData
+import kotlinx.android.synthetic.main.search_fragment.*
 
 
 class SearchFragment : Fragment() {
@@ -18,7 +21,14 @@ class SearchFragment : Fragment() {
     private lateinit var viewModel: SearchViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        search_fragment_recycler_view.adapter = SearchAdapter(CompanyData().name)
+        search_fragment_recycler_view.layoutManager = LinearLayoutManager(Fragment().context)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
