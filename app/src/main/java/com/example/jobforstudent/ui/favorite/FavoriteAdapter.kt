860a2872e54af11c80609661c51b7a7aee8f96f5
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-05-12 13:20:59 +0300

    * add OnClick image

--- app/src/main/java/com/example/jobforstudent/ui/favorite/FavoriteAdapter.kt
+++ app/src/main/java/com/example/jobforstudent/ui/favorite/FavoriteAdapter.kt
@@ -3,6 +3,7 @@ package com.example.jobforstudent.ui.favorite
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jobforstudent.CompanyData
@@ -14,6 +15,7 @@ class FavoriteAdapter(private val data: CompanyData) : RecyclerView.Adapter<Favo
     class FavoriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nameText: TextView = itemView.findViewById(R.id.item_advert_company_name_text)
         var locationText: TextView = itemView.findViewById(R.id.item_advert_location_text)
+        val favoriteImage: ImageView = itemView.findViewById(R.id.item_advert_favorite_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
@@ -26,5 +28,6 @@ class FavoriteAdapter(private val data: CompanyData) : RecyclerView.Adapter<Favo
     override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
         holder.nameText.text = data.name[position]
         holder.locationText.text = data.location[position]
+        holder.favoriteImage.setImageResource(R.drawable.ic_favorite_true_black_24dp)
     }
 }
--- app/src/main/java/com/example/jobforstudent/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/jobforstudent/ui/search/SearchAdapter.kt
@@ -3,6 +3,7 @@ package com.example.jobforstudent.ui.search
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jobforstudent.CompanyData
@@ -14,6 +15,8 @@ class SearchAdapter(private val data: CompanyData) : RecyclerView.Adapter<Search
     class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nameText: TextView = itemView.findViewById(R.id.item_advert_company_name_text)
         var locationText: TextView = itemView.findViewById(R.id.item_advert_location_text)
+        val favoriteImage: ImageView = itemView.findViewById(R.id.item_advert_favorite_image)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
@@ -26,5 +29,8 @@ class SearchAdapter(private val data: CompanyData) : RecyclerView.Adapter<Search
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
         holder.nameText.text = data.name[position]
         holder.locationText.text = data.location[position]
+        holder.favoriteImage.setOnClickListener {
+            holder.favoriteImage.setImageResource(R.drawable.ic_favorite_true_black_24dp)
+        }
     }
 }
