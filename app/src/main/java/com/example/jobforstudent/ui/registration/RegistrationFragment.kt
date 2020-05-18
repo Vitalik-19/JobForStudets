dfce33421b89352a23d526bca32e620df6b875a5
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-05-18 14:02:42 +0300

    * initialization data binding fragments * edited id element in fragments * update navigation

--- app/src/main/java/com/example/jobforstudent/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/jobforstudent/ui/registration/RegistrationFragment.kt
@@ -4,30 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import com.example.jobforstudent.R
+import com.example.jobforstudent.databinding.RegistrationFragmentBinding
 
 class RegistrationFragment : Fragment() {
 
     companion object {
         fun newInstance() =
-            RegistrationFragment()
+                RegistrationFragment()
     }
 
     private lateinit var viewModel: RegistrationViewModel
+    private lateinit var binding: RegistrationFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.registration_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.registration_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(RegistrationViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+        binding.registrationViewModel = viewModel
+        binding.setLifecycleOwner(this)
+
+        //todo verification registration
+        binding.registrationFragmentRegistrationButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_registrationFragment_to_loginFragment)
+        )
+
+        return binding.root
+    }
 }
