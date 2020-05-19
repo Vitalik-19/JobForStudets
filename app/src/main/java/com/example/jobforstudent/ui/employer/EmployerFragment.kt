bae8d27ea430ea9e832ead0d3ad92b963f658562
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-05-19 17:55:46 +0300

    * update popUpTo

--- app/src/main/java/com/example/jobforstudent/ui/employer/EmployerFragment.kt
+++ app/src/main/java/com/example/jobforstudent/ui/employer/EmployerFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
