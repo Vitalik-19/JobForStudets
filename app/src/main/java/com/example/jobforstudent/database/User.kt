aa6b2beca7ae07f3a31d5f1a14294c1f006b9f6a
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-05-21 05:26:48 +0300

    * reworked entities and their connections * fixed singIn * updated loginFragment

--- app/src/main/java/com/example/jobforstudent/database/User.kt
+++ app/src/main/java/com/example/jobforstudent/database/User.kt
@@ -3,14 +3,6 @@ package com.example.jobforstudent.database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
-data class User(
-        @PrimaryKey(autoGenerate = true)
-        val userId: Long = 0L,
-        var position: Boolean = true,
-        var employersId: Long = 0L,
-        var seekersId: Long = 0L
-)
 
 @Entity
 data class Employer(
--- app/src/main/java/com/example/jobforstudent/database/UserDatabaseDao.kt
+++ app/src/main/java/com/example/jobforstudent/database/UserDatabaseDao.kt
@@ -1,14 +1,13 @@
 package com.example.jobforstudent.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
 
 @Dao
 public interface UserDatabaseDao {
-
-    // Добавление user в бд
-    @Insert
-    fun insertUser(user: User)
 
     @Insert
     fun insertEmployer(employer: Employer)
@@ -16,33 +15,17 @@ public interface UserDatabaseDao {
     @Insert
     fun insertSeeker(seeker: Seeker)
 
-    // Удаление user из бд
-    @Query("DELETE FROM User")
-    fun clear()
-
-    // Обновление user в бд
-    @Update
-    fun update(user: User)
-
     @Query("SELECT * FROM Employer")
     fun getAllEmployer(): LiveData<List<Employer>>
 
-    @Query("SELECT * FROM Employer WHERE loginEmployer = :login")
-    fun getEmployer(login: String): Employer?
+    @Query("SELECT * FROM Employer WHERE loginEmployer = :login AND password = :password")
+    fun getEmployer(login: String, password: String): Employer?
 
     @Query("SELECT * FROM Seeker")
     fun getAllSeeker(): LiveData<List<Seeker>>
 
-    @Query("SELECT * FROM Seeker WHERE loginSeeker = :login")
-    fun getSeeker(login: String): Seeker?
-
-    @Transaction
-    @Query("SELECT * FROM User")
-    fun getUserWithEmployers(): List<UserWithEmployers>
-
-    @Transaction
-    @Query("SELECT * FROM User")
-    fun getUserWithSeekers(): List<UserWithSeekers>
+    @Query("SELECT * FROM Seeker WHERE loginSeeker = :login AND password = :password")
+    fun getSeeker(login: String, password: String): Seeker?
 
     @Transaction
     @Query("SELECT * FROM Employer")
@@ -51,5 +34,4 @@ public interface UserDatabaseDao {
     @Transaction
     @Query("SELECT * FROM Seeker")
     fun getSeekerWithAdverts(): List<SeekerWithAdverts>
-
 }
